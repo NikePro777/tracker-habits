0f7f6f2ade0eda8736fe7c4c805561629b831be7
Evgeniy Datskij <evgenij.datskij@mail.ru>
2022-09-21 21:36:28 +0500

    bags

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,5 +48,21 @@ button.forEach((btn) =>
     toggleHabit(target);
   })
 );
+
 // console.log(button[6].id);
+eekDays
+  .map((name, index) =>
+    completed[index]
+      ? `<button class="checked bg-black rounded-full h-12 w-12 flex items-center justify-center" id=${id}${index}><img width="45" src="./assets/images/check.svg" alt="check" /></button>`
+      : `<button class="rounded-full border-2 border-solid transition-opacity opacity-20 border-black h-12 w-12 flex items-center justify-center text-lg uppercase font-semibold unchecked" id=${id}${index}>${name}</button>`
+  )
+  .join("");
+
+const getHabitElement = ({ img, name, completed }, index) =>
+  `<div class="mb-8"><div class="flex items-center gap-4 mb-5"><img width="70" class="rounded-3xl border-2 border-[#e2e4dd] border-solid" src="${img}" alt="no coffee"/>
+  <span class="font-semibold text-2xl">${name}</span></div><div class="flex items-center justify-between habit-plan">${getWeekDaysElement(
+    completed,
+    index
+  )}
+  </div></div>`;
 // habitContainer.button.addEventListener('click'=toggleHabit)
