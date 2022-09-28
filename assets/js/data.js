19819d64fe0e9eb236cebecd257e876ee7349c18
Evgeniy Datskij <evgenij.datskij@mail.ru>
2022-09-28 23:53:16 +0500

    reback

--- assets/js/data.js
+++ assets/js/data.js
@@ -5,14 +5,4 @@ export const habits = [
     name: "No caffeine",
     completed: [true, false, false, false, true, false, true],
   },
-  {
-    img: "./assets/images/coffee.png",
-    name: "true eat",
-    completed: [false, false, false, false, true, false, true],
-  },
-  {
-    img: "./assets/images/coffee.png",
-    name: "Sport",
-    completed: [true, false, false, false, true, false, true],
-  },
 ];
