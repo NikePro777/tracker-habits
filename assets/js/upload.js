7a6330d126104a7893b4731e0270c7c4b0fda713
Evgeniy Datskij <evgenij.datskij@mail.ru>
2022-10-03 21:35:13 +0500

    add preview image

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,5 +1,7 @@
 export function upload(selector, options = {}) {
   const input = document.querySelector(selector);
+  const preview = document.createElement("div");
+  preview.classList.add("preview");
   const open = document.createElement("button");
   open.classList.add("btn");
   open.textContent = "open";
@@ -13,12 +15,33 @@ export function upload(selector, options = {}) {
     input.setAttribute("accept", options.accept.join(","));
   }
   // помещяем наш элемент в конец селектора
+  input.insertAdjacentElement("afterend", preview); //afterend означает что контент будет находиться вне нашего блока
   input.insertAdjacentElement("afterend", open);
 
   // Функция которая по клике на нашу кнопку, вызывает клик по импуту (который открывает вкладку по загрузке файлов)
   const triggerInput = () => input.click();
   const changeHandler = (event) => {
-    console.log(event.target.files);
+    if (!event.target.files.length) {
+      return;
+    }
+    //изначально event.target.files это толи какой то особой тип толи обьект, мы его приводим к массиву
+    const files = Array.from(event.target.files);
+    files.forEach((file) => {
+      if (!file.type.match("image")) {
+        // работать будем только с картинкой
+        return;
+      }
+      const reader = new FileReader();
+      reader.onload = (ev) => {
+        const src = ev.target.result;
+        preview.insertAdjacentHTML(
+          "afterbegin",
+          `<div class='preview-image'> <img src='${src}' alt='${file.name}'/></div>`
+        );
+      };
+      reader.readAsDataURL(file);
+    });
+    console.log(files);
   };
   open.addEventListener("click", triggerInput);
   input.addEventListener("change", changeHandler);
